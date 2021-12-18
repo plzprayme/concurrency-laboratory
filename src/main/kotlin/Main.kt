7294c8c65e21dcab7ba89bb575080858e410e6ad
prayme <dbfpzk142@gmail.com>
2021-12-19 03:38:00 +0900

    Description 참고
    1 스레드 + 2 코루틴으로 BusyJob을 수행하면 1스레드 1코루틴과 차이가 전혀 없다. 순차적으로 실행된다. 코루틴 디스패처에 의해서 메인 스레드에 BusyJob이 두개가 할당되고, 순차적으로 수행이 되는 것 같다.
    2 스레드 + 2 코루틴으로 BusyJob을 수행하면 빠르다.
    
    그렇다면 1 스레드 + 2 코루틴 LazyJob을 수행하면 어떻게 될까? CPU 타임이 필요 없는, NetworkIO나 FileIO 예제를 작성해보자.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,109 @@
-fun main(args : Array<String>) {
-    args.forEach { print(it) }
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+import java.lang.Thread.sleep
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+
+fun main(args: Array<String>) {
+    Thread.currentThread().threadGroup.list()
+
+    printSpendTime {
+        when (args[0]) {
+            "st" -> singleThread(busyJob)
+            "stsc" -> singleThreadSingleCoroutine(busyJob)
+            "stscs" -> singleThreadSplitVersion(busyJob)
+            "stdc" -> singleThreadDoubleCoroutines(busyJob)
+            "dtdc" -> doubleThreadDoubleCoroutines(busyJob)
+            "lstdc" -> singleThreadDoubleCoroutines(lazyJob)
+        }
+    }
+}
+
+fun singleThread(job: (Int, Int) -> Int) {
+    // 3.0초
+    val sum = job(Int.MIN_VALUE, Int.MAX_VALUE)
+    println("SINGLE THREAD RESULT: $sum")
+}
+
+fun singleThreadSplitVersion(job: (Int, Int) -> Int) {
+    // JOB을 나누면 5.1초정도 걸리네..
+    var sum = 0;
+    sum += job(Int.MIN_VALUE, 0)
+    sum += job(0, Int.MAX_VALUE)
+    println("SINGLE THREAD SINGLE COROUTINE SPLIT VERSION RESULT: $sum")
+}
+
+fun singleThreadSingleCoroutine(job: (Int, Int) -> Int) {
+    // 3.1초
+    runBlocking {
+        launch {
+            val sum = job(Int.MIN_VALUE, Int.MAX_VALUE)
+            println("SINGLE THREAD SINGLE COROUTINE RESULT: $sum")
+        }
+    }
+
+}
+
+fun singleThreadDoubleCoroutines(job: (Int, Int) -> Int) {
+    // 싱글 스레드 더블 코루틴: 5.5초
+    var sum = 0
+
+    runBlocking {
+
+        launch {
+            println("${Thread.currentThread()}: FIRST LAUNCH START")
+            sum += job(Int.MIN_VALUE, 0)
+            println("${Thread.currentThread()}: FIRST LAUNCH END")
+        }
+
+        launch {
+            println("${Thread.currentThread()}: SECOND LAUNCH START")
+            sum += job(0, Int.MAX_VALUE)
+            println("${Thread.currentThread()}: SECOND LAUNCH END")
+        }
+
+    }
+
+    println("SINGLE THREAD DOUBLE COROUTINE RESULT: $sum")
+
+}
+
+fun doubleThreadDoubleCoroutines(job: (Int, Int) -> Int) {
+    // 더블 스레드 더블 코루틴: 1.69초
+    var sum = 0
+
+    runBlocking {
+
+        launch {
+            println("${Thread.currentThread()}: FIRST LAUNCH START")
+            sum += job(Int.MIN_VALUE, 0)
+            println("${Thread.currentThread()}: FIRST LAUNCH END")
+        }
+
+        launch(newSingleThreadContext("NewThread")) {
+            println("${Thread.currentThread()}: FIRST LAUNCH START")
+            sum += job(0, Int.MAX_VALUE)
+            println("${Thread.currentThread()}: FIRST LAUNCH END")
+        }
+
+    }
+
+    println("SINGLE THREAD DOUBLE COROUTINE RESULT: $sum")
+
+}
+
+@OptIn(ExperimentalTime::class)
+fun printSpendTime(job: () -> Unit) {
+    val duration = measureTime { job() }
+    println("Spend Time: $duration")
+}
+
+val busyJob = { start: Int, endInclusive: Int ->
+    IntRange(start, endInclusive).sumOf { it }
+}
+
+val lazyJob = { _: Int, _: Int ->
+    sleep(5_000)
+    1
 }
